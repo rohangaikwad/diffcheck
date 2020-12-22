3dc56823958052d70321723a9d6f1279cbc536a1
rohangaikwad <rohan.gaikwad@powerweave.com>
2020-12-23 00:09:22 +0530

    languages

--- js/main.js
+++ js/main.js
@@ -14,11 +14,24 @@ require(['vs/editor/editor.main'], function () {
 
 });
 
+let lang = document.getElementById('lang');
+lang.addEventListener('change', () => {
+    [orgEditor, newEditor].forEach(_editor => {
+        let data = _editor.getValue();
+        var oldModel = _editor.getModel();
+        var newModel = monaco.editor.createModel(data, lang.value);
+        _editor.setModel(newModel);
+        if (oldModel) {
+            oldModel.dispose();
+        }
+    });
+})
+
 document.querySelector('.src.s1 button').addEventListener('click', () => {
     let url1 = document.getElementById('url1');
     console.log(url1.value)
     if (url1.value.length > 0) {
-        
+
         fetch(url1)
             .then(res => res.text())
             .then(res => {
@@ -35,7 +48,7 @@ document.querySelector('.src.s2 button').addEventListener('click', () => {
     console.log('url 1')
     let url2 = document.getElementById('url2');
     if (url2.value.length > 0) {
-        
+
         fetch(url2)
             .then(res => res.text())
             .then(res => {
@@ -50,8 +63,8 @@ document.querySelector('.src.s2 button').addEventListener('click', () => {
 
 document.querySelector('#compare').addEventListener('click', () => {
     diffEditor.setModel({
-        original: monaco.editor.createModel(orgEditor.getValue(), "text/plain"),
-        modified: monaco.editor.createModel(newEditor.getValue(), "text/plain")
+        original: monaco.editor.createModel(orgEditor.getValue(), lang.value),
+        modified: monaco.editor.createModel(newEditor.getValue(), lang.value)
     });
     document.querySelector('#lr2').click();
     document.querySelector('.diff-review-actions .close-diff-review').click();
